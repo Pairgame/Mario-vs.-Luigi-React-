244cdf32e100a46cc184591f0ec92d4147c7b540
Claudia413 <cmjengelsman@gmail.com>
2017-06-20 16:02:26 +0200

    Add Sass properly

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+<<<<<<< HEAD
 
 
+=======
+import './App.css';
+>>>>>>> 25dbc0ae33a89b623e36b4d77bd717e7cd090f0b
 
 class App extends Component {
   render() {
