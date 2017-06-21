5e9caee3eac46cdd2ca3dcbeef7bfc15abe485c6
Claudia413 <cmjengelsman@gmail.com>
2017-06-21 10:49:25 +0200

    Fix errors, added feathers to package JSON and put index in provider for redux

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute } from 'react-router'
-import App from './App';
-import store, { history } from './store'
-import registerServiceWorker from './registerServiceWorker';
-import injectTapEventPlugin from 'react-tap-event-plugin'
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from 'react-redux'
+import { Router, Route, IndexRoute } from "react-router";
+import App from "./App";
+import store, { history } from "./store";
+import registerServiceWorker from "./registerServiceWorker";
+import injectTapEventPlugin from "react-tap-event-plugin";
 // import './index.css';
 
 injectTapEventPlugin();
 
 ReactDOM.render(
-  <App>
-    <Route path="/" component={App}>
-    </Route>
-  </App>,
-document.getElementById('root'));
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App} />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
