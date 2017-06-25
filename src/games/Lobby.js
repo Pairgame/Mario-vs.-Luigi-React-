dba3d0387871c073f72ce951653823899a655e20
Claudia413 <cmjengelsman@gmail.com>
2017-06-25 22:18:22 +0200

    fix name of game and add start score for both players

--- src/games/Lobby.js
+++ src/games/Lobby.js
@@ -47,7 +47,7 @@ class Lobby extends PureComponent {
           key={index}
           onClick={this.goToGame(game._id).bind(this)}
           rightIcon={<ActionIcon />}
-          primaryText={`Mario's Game`} />
+          primaryText={game.name} />
       )
     }
 
